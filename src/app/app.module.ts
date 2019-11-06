0fdd0c14efbd1ea3f3e03276795d9704a96d5445
elias rosales <elias.rosales@unidadeditorial.es>
2019-11-06 12:52:47 +0100

    Adding Angular Custom Element without zone.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ButtonComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [ButtonComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(private injector: Injector){}
+  ngDoBootstrap(){
+    const elements: any[] = [
+      [ButtonComponent, "ue-button"]
+    ];
+
+
+    for (const [component, name] of elements) {
+      const el = createCustomElement(component, { injector: this.injector });
+      customElements.define(name, el);
+    }
+
+  }
+}
